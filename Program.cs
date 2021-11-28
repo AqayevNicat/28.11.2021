4ceba979c4c352b0694e80437a54c1c6f7078fa9
AqayevNicat <nijatam@code.edu.az>
2021-11-28 13:41:21 +0400

    Project1-C#

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region--Task1
+            //int number = 46;
+            //int count = 0;
+            //for (int i = 1; i  <=number; i ++)
+            //{
+            //    if (number % i == 0)
+            //    {
+            //        count++;
+            //    }
+            //}
+            //if (count > 2)
+            //{
+            //    Console.WriteLine(number + " murekkeb ededdir");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(number + " sade ededdir");
+            //}
+            #endregion
+
+            #region--Task2
+            //int number = 199;
+            //int sum = 0;
+            //for(int onluq = 1; onluq <= number; onluq*=10)
+            //{
+            //    sum  += (number / onluq) % 10;
+            //}
+            //Console.WriteLine(sum);
+            #endregion
+
+            #region-Task3
+            //int number = 12;
+            //switch (number)
+            //{
+            //    case 12:
+            //    case 1:
+            //    case 2:
+            //        Console.WriteLine("Qis feslidir");
+            //        break;
+            //    case 3:
+            //    case 4:
+            //    case 5:
+            //        Console.WriteLine("Yaz feslidir");
+            //        break;
+            //    case 6:
+            //    case 7:
+            //    case 8:
+            //        Console.WriteLine("Yay feslidir");
+            //        break;
+            //    case 9:
+            //    case 10:
+            //    case 11:
+            //        Console.WriteLine("Payiz feslidir");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Duzgun eded daxil edin");
+            //        break;
+            //}
+            #endregion
+        }
+    }
+}
